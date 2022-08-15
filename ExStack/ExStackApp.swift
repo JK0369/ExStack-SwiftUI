fde3818bc691269f7883e327338d306577a96aeb
jake <palatable7@naver.com>
2022-08-15 16:54:28 +0900

    Initial Commit

---
+++ ExStack/ExStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExStackApp.swift
+//  ExStack
+//
+//  Created by 김종권 on 2022/08/15.
+//
+
+import SwiftUI
+
+@main
+struct ExStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
